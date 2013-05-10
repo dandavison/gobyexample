65f5028721b693e3eea4fbe137c27f51950b2671
Dan Davison <dan@counsyl.com>
2013-05-10 01:28:16 -0700

    Implement measure within class definition

--- examples/interfaces/interfaces2.hs
+++ examples/interfaces/interfaces2.hs
@@ -1,11 +1,16 @@
 class Geometry g where
   area  :: g -> Float
   perim :: g -> Float
+  measure :: (Show g) => g -> IO ()
+
+  measure g = do
+    print g
+    print $ area g
+    print $ perim g
 
 
 data Square = Square Float Float deriving (Show)
 data Circle = Circle Float deriving (Show)
-
 
 instance Geometry Square where
   area (Square w h) = w * h
@@ -14,13 +19,6 @@ instance Geometry Square where
 instance Geometry Circle where
   area (Circle r) = pi * r ^ 2
   perim (Circle r) = pi * 2 * r
-
-
-measure :: (Geometry g) => (Show g) => g -> IO ()
-measure g = do
-  print g
-  print $ area g
-  print $ perim g
 
 
 main = do
